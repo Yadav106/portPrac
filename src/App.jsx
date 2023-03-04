2ad9b6185f4a01752970404bfc4c14c16b6523a7
Yadav106 <sahil.jp.yadav@gmail.com>
2023-03-04 13:56:22 +0530

    Added TailWindCSS

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 
 const App = () => {
   return (
-    <div className="App">App</div>
+    <h1 className="text-3xl font-bold underline text-center text-red-600">Hello Tailwind!</h1>
   )
 }
 
