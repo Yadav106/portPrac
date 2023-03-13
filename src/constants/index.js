3dc84ded244cf5043b3300c515cac5180caf9672
Yadav106 <sahil.jp.yadav@gmail.com>
2023-03-13 18:55:09 +0530

    tech cleanup

--- src/constants/index.js
+++ src/constants/index.js
@@ -73,10 +73,6 @@ import {
       icon: reactjs,
     },
     {
-      name: "Redux Toolkit",
-      icon: redux,
-    },
-    {
       name: "Tailwind CSS",
       icon: tailwind,
     },
@@ -85,24 +81,12 @@ import {
       icon: nodejs,
     },
     {
-      name: "MongoDB",
-      icon: mongodb,
-    },
-    {
       name: "Three JS",
       icon: threejs,
     },
     {
       name: "git",
       icon: git,
-    },
-    {
-      name: "figma",
-      icon: figma,
-    },
-    {
-      name: "docker",
-      icon: docker,
     },
   ];
   
